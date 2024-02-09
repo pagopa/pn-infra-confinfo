cc666d1ef8fe635874c2d41b8f25d8e69197958b
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-02-09 17:28:51 +0100

    File format

--- functions/diagnostic-data-proxy/src/app/config.js
+++ functions/diagnostic-data-proxy/src/app/config.js
@@ -1,6 +1,6 @@
 /**
  * Retrieves the value of an environment variable.
- * 
+ *
  * @param {string} env - The name of the environment variable to retrieve.
  * @return {string} The value of the specified environment variable.
  * @throws {Error} When the environment variable is not defined.
@@ -15,21 +15,23 @@ const getEnvironmentVariable = (env) => {
 
 /**
  * Gets the safestorage bucket name from environment variables.
- * 
+ *
  * @return {string} The storage bucket name.
  */
-export const getSafeStorageBucket = () => getEnvironmentVariable("SAFESTORAGE_BUCKET");
+export const getSafeStorageBucket = () =>
+  getEnvironmentVariable('SAFESTORAGE_BUCKET');
 
 /**
  * Get the AWS region for DynamoDB from environment variables.
- * 
+ *
  * @return {string} The AWS region for DynamoDB.
  */
-export const getDynamoAWSRegion = () => getEnvironmentVariable("DYNAMO_AWS_REGION");
+export const getDynamoAWSRegion = () =>
+  getEnvironmentVariable('DYNAMO_AWS_REGION');
 
 /**
  * Gets the AWS region for S3 services from environment variables.
- * 
+ *
  * @return {string} The AWS region for S3.
  */
-export const getS3AWSRegion = () => getEnvironmentVariable("S3_AWS_REGION");
+export const getS3AWSRegion = () => getEnvironmentVariable('S3_AWS_REGION');
