89e1eb2b8a8f8e17d9f11e68cb794d12dbcb6e27
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-02-09 17:30:19 +0100

    Refactor

--- functions/diagnostic-data-proxy/src/app/eventHandler.js
+++ functions/diagnostic-data-proxy/src/app/eventHandler.js
@@ -1,45 +1,45 @@
-import { getObject, headObject } from "./safestorage.js";
-import { getEvents } from "./ecRichiesteMetadati.js";
+import { getObject, headObject } from './safestorage.js';
+import { getEvents } from './ecRichiesteMetadati.js';
+import { checkKeys, makeResponse } from './utils.js';
 
 /**
  * Handles an event by executing different actions based on the event's details.
  *
- * @param {Object} event - The event to handle, containing an action and a payload.
- * @return {Promise<Object>} A promise that resolves to an object with a statusCode and a body.
- *                           The body contains the result of the action, if successful.
+ * @param {Object} event - The event to handle, containing an action and a
+ * payload.
+ * @return {Promise<Object>} A promise that resolves to an object with a
+ * statusCode and a body. The body contains the result of the action, if
+ * successful.
  */
 export const handleEvent = async (event) => {
-  const { action, payload } = event;
-  
-  if( action === "GET_OBJECT") {
-    try {
-      let body = await getObject(payload.aarKey);
-      return {
-        statusCode: 200,
-        body
-      };
-    } catch (err) {
-      return {
-        statusCode: 400,
-      };
-    }
-  }else if( action === "HEAD_OBJECT") {
-    let body = await headObject(payload.aarKey);
-    return {
-      statusCode: (body.found) ? 200 : 404,
-      body
-    };
-  } else if( action === "GET_EC_RICHIESTE_METADATI") {
-    const checkRetry = payload.checkRetry === undefined ? 
-                                  false : payload.checkRetry;
-    let body = await getEvents(payload.requestId, checkRetry);
-    return {
-      statusCode: 200,
-      body
-    };
+  try {
+    checkKeys(event, [['action', 'string']]);
+  } catch (e) {
+    return makeResponse(400, `Invalid request: ${e.message}`);
   }
-  return {
-    statusCode: 200,
-    body: 'I am alive!',
-  };
+
+  switch (event.action) {
+    case 'GET_OBJECT':
+    case 'HEAD_OBJECT':
+      try {
+        const { fileKey } = checkKeys(event, [['fileKey', 'string']]);
+        let res =
+          event.action === 'HEAD_OBJECT'
+            ? await headObject(fileKey)
+            : await getObject(fileKey);
+        return makeResponse(res.found ? 200 : 404, res);
+      } catch (e) {
+        return makeResponse(400, e.message);
+      }
+    case 'GET_EC_RICHIESTE_METADATI':
+      try {
+        const { requestId } = checkKeys(event, [['requestId', 'string']]);
+        const { checkRetry = false } = event;
+        let res = await getEvents(requestId, checkRetry);
+        return makeResponse(200, res);
+      } catch (e) {
+        return makeResponse(400, e.message);
+      }
+  }
+  return makeResponse(501, `Invalid action!`);
 };
