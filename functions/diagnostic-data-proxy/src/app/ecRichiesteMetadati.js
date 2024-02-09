c7609a6e322ee9c1dd2625a881a7b9685f6ccd26
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-02-09 17:29:44 +0100

    Format docs

--- functions/diagnostic-data-proxy/src/app/ecRichiesteMetadati.js
+++ functions/diagnostic-data-proxy/src/app/ecRichiesteMetadati.js
@@ -8,9 +8,10 @@ const tableName = 'pn-EcRichiesteMetadati';
 
 /**
  * Fetches an event by its requestId from DynamoDB.
- * 
+ *
  * @param {string} requestId - The unique identifier for the request.
- * @return {Promise<Object>} The event data associated with the requestId, if found.
+ * @return {Promise<Object>} The event data associated with the requestId, if
+ * found.
  */
 export const getEvent = async (requestId) => {
   const command = new GetCommand({
@@ -24,14 +25,15 @@ export const getEvent = async (requestId) => {
 
 
 /**
-  * Retrieves events from DynamoDB based on a requestId. If `checkRetry` is enabled,
-  * it attempts to fetch additional events by appending a .PCRETRY_0, .PCRETRY_1 etc.
+  * Retrieves events from DynamoDB based on a requestId. If `checkRetry` is
+  * enabled, it attempts to fetch additional events by appending a .PCRETRY_0,
+  * .PCRETRY_1 etc.
   *
   * @param {string} requestId - The base requestId for the event(s) to fetch.
-  * @param {boolean} checkRetry - A flag to determine whether to perform retries. If true,
-  *                                the function will attempt to fetch events with .PCRETRY_i.
-  * @return {Promise<Array>} A promise that resolves to an array of events related to the requestId.
-  *                          If no events are found it returns an empty array.
+  * @param {boolean} checkRetry - A flag to determine whether to perform
+  * retries. If true, the function will attempt to fetch events with .PCRETRY_i.
+  * @return {Promise<Array>} A promise that resolves to an array of events
+  * related to the requestId. If no events are found it returns an empty array.
   */
 export const getEvents = async (requestId, checkRetry) => {
   let requestIdRetry = requestId;
