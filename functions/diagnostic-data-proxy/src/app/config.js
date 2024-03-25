afb5c841b136bccc4bc4bc0b498092813e0bc6dc
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-03-25 15:34:50 +0100

    feat: add functions to retrieve Glacier restore days and tier.

--- functions/diagnostic-data-proxy/src/app/config.js
+++ functions/diagnostic-data-proxy/src/app/config.js
@@ -3,15 +3,8 @@
  *
  * @param {string} env - The name of the environment variable to retrieve.
  * @return {string} The value of the specified environment variable.
- * @throws {Error} When the environment variable is not defined.
  */
-const getEnvironmentVariable = (env) => {
-  const value = process.env[env];
-  if (!value) {
-    throw new Error(`Environment variable "${env}" is not defined.`);
-  }
-  return value;
-};
+const getEnvironmentVariable = (env) => process.env[env];
 
 /**
  * Gets the safestorage bucket name from environment variables.
@@ -37,14 +30,42 @@ export const getDynamoAWSRegion = () =>
 export const getS3AWSRegion = () => getEnvironmentVariable('S3_AWS_REGION');
 
 /**
-* Gets the generated url expiration in seconds.
-*
-* @return {number} The url expiration in seconds, undefined otherwise.
-*/
+ * Gets the generated url expiration in seconds.
+ *
+ * @return {number} The url expiration in seconds, undefined otherwise.
+ */
 export const getUrlExpiresSeconds = () => {
-  try{
-    return parseInt(getEnvironmentVariable('URL_EXPIRES_SECONDS'));
-  } catch (e){
+  const value = parseInt(getEnvironmentVariable('URL_EXPIRES_SECONDS'));
+  if (isNaN(value)) {
     return undefined;
   }
+  return value;
 };
+
+/**
+ * Retrieves the number of days for which a Glacier restored object will remain
+ * in the S3 Standard storage class before being moved back to Glacier.
+ *
+ * @returns {number | undefined} The number of days to keep the restored object
+ * in S3 Standard storage class as an integer, `undefined` otherwise.
+ */
+export const getGlacierRestoreDays = () => {
+  const value = parseInt(getEnvironmentVariable('GLACIER_RESTORE_DAYS'));
+  if (isNaN(value)) {
+    return undefined;
+  }
+  return value;
+};
+
+
+/**
+ * Retrieves the tier to use for a Glacier restore job. The value is determined
+ * by an environment variable. Valid tiers include 'Standard', 'Bulk', and
+ * 'Expedited'. The choice of tier affects the cost and speed of the restore
+ * operation.
+ *
+ * @returns {string} The Glacier restore tier, one of 'Standard', 'Bulk', or
+ * 'Expedited'.
+ */
+export const getGlacierRestoreTier = () =>
+  getEnvironmentVariable('GLACIER_RESTORE_TIER');
