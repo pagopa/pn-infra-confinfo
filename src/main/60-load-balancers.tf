34d69c055b60e6e743b6e6cbaf08ed515aad63e9
Alessio Angelotti <alessio.angelotti@pagopa.it>
2026-06-12 17:29:56 +0200

    PN-19194: align Cons-Norm routing to dedicated listener

--- src/main/60-load-balancers.tf
+++ src/main/60-load-balancers.tf
@@ -289,21 +289,17 @@ resource "aws_lb_listener" "pn_confinfo_postel_nlb_http_to_alb_http" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.pn_confinfo_postel_nlb_http_to_alb_http.arn
+    target_group_arn = aws_lb_target_group.pn_confinfo_postel_nlb_http_to_dedicated_alb_listener_http.arn
   }
 }
-# - Postel NLB target group for dedicated HTTP listener
-resource "aws_lb_target_group" "pn_confinfo_postel_nlb_http_to_alb_http" {
-  name_prefix = "PtI-"
+# - Postel NLB target group for dedicated ALB listener
+resource "aws_lb_target_group" "pn_confinfo_postel_nlb_http_to_dedicated_alb_listener_http" {
+  name_prefix = "PtP-"
   vpc_id      = module.vpc_pn_confinfo.vpc_id
 
   port        = var.cons_norm_private_link_listener_port
   protocol    = "TCP"
   target_type = "alb"
-
-  lifecycle {
-    create_before_destroy = true
-  }
    
   depends_on = [
     aws_lb.pn_confinfo_postel_nlb,
@@ -312,7 +308,7 @@ resource "aws_lb_target_group" "pn_confinfo_postel_nlb_http_to_alb_http" {
   ]
 
   tags = {
-    "Description": "PN Confinfo - ConsNorm PrivateLink channel to dedicated ALB listener - Target Group"
+    "Description": "PN Confinfo - Postel NLB to dedicated ALB listener - Target Group"
   }
 
   health_check {
@@ -321,15 +317,11 @@ resource "aws_lb_target_group" "pn_confinfo_postel_nlb_http_to_alb_http" {
   }
 }
 # - Postel NLB target group for HTTP attachmet
-resource "aws_lb_target_group_attachment" "pn_confinfo_postel_nlb_http_to_alb_http" {
-  target_group_arn  = aws_lb_target_group.pn_confinfo_postel_nlb_http_to_alb_http.arn
+resource "aws_lb_target_group_attachment" "pn_confinfo_postel_nlb_http_to_dedicated_alb_listener_http" {
+  target_group_arn  = aws_lb_target_group.pn_confinfo_postel_nlb_http_to_dedicated_alb_listener_http.arn
   port              = var.cons_norm_private_link_listener_port
 
   target_id         = aws_lb.pn_confinfo_ecs_alb.arn
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
 # - Postel NLB listener for HTTPS
